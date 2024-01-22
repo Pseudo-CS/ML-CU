debbef97d001cfcdd6f2c94349b1e30f605e036c
Botakun02 <154114989+Botakun02@users.noreply.github.com>
2024-01-22 09:47:19 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -54,32 +54,34 @@ def del_sc1():
 
 
 # error message for name and no
-def err_screen():
+def err_screen(): # Creating a new window 'sc1' using the 'tk.Tk()' command from the tkinter library
     global sc1
-    sc1 = tk.Tk()
-    sc1.geometry("400x110")
-    sc1.iconbitmap("AMS.ico")
-    sc1.title("Warning!!")
-    sc1.configure(background="black")
-    sc1.resizable(0, 0)
-    tk.Label(
-        sc1,
-        text="Enrollment & Name required!!!",
-        fg="yellow",
-        bg="black",
-        font=("times", 20, " bold "),
-    ).pack()
-    tk.Button(
-        sc1,
-        text="OK",
-        command=del_sc1,
-        fg="yellow",
-        bg="black",
-        width=9,
-        height=1,
-        activebackground="Red",
-        font=("times", 20, " bold "),
-    ).place(x=110, y=50)
+    sc1 = tk.Tk() # creates a new Tk root window
+    sc1.geometry("400x110") # sets the geometry of the window to 400x110 pixels
+    sc1.iconbitmap("AMS.ico") # sets the icon of the window to the file "AMS.ico"
+    sc1.title("Warning!!") # sets the title of the window to "Warning!!"
+    sc1.configure(background="black") # sets the background color of the window to black
+    sc1.resizable(0, 0) # sets the window to be not resizable
+   # Adding a label with specific text, color, background color, and font to the window
+    tk.Label(  # creates a new label
+        sc1, # specifies the parent window of the label
+        text="Enrollment & Name required!!!",  # sets the text of the label
+        fg="yellow",  # sets the foreground color of the label to yellow
+        bg="black", # sets the background color of the label to black
+        font=("times", 20, " bold "),  # sets the font of the label to Times New Roman, 20 point, bold
+    ).pack() # packs the label into the parent window
+   # Adding a button with specific text, color, background color, and font to the window
+    tk.Button( # creates a new button
+        sc1,  # specifies the parent window of the button
+        text="OK", # sets the text of the button to "OK"
+        command=del_sc1, # sets the command that will be executed when the button is clicked
+        fg="yellow",  # sets the foreground color of the button to yellow
+        bg="black", # sets the background color of the button to black
+        width=9, # sets the width of the button to 9 characters
+        height=1, # sets the height of the button to 1 character
+        activebackground="Red", # sets the background color of the button when it is active (clicked) to red
+        font=("times", 20, " bold "),  # sets the font of the button to Times New Roman, 20 point, bold
+    ).place(x=110, y=50) # places the button at the position x=110, y=50 in the parent window
 
 
 def testVal(inStr, acttyp):
@@ -89,63 +91,62 @@ def testVal(inStr, acttyp):
     return True
 
 
-logo = Image.open("UI_Image/0001.png")
-logo = logo.resize((50, 47), Image.LANCZOS)
-logo1 = ImageTk.PhotoImage(logo)
-titl = tk.Label(window, bg="black", relief=RIDGE, bd=10, font=("arial", 35))
-titl.pack(fill=X)
-l1 = tk.Label(window, image=logo1, bg="black",)
-l1.place(x=470, y=10)
+logo = Image.open("UI_Image/0001.png") #Opening the logo image
+logo = logo.resize((50, 47), Image.LANCZOS) #Resizing the logo image to (50, 47) pixels using the Lanczos resampling filter
+logo1 = ImageTk.PhotoImage(logo) #Converting the resized logo image to a PhotoImage object and saving it in the logo1 variable
+titl = tk.Label(window, bg="black", relief=RIDGE, bd=10, font=("arial", 35)) #Creating a label with a black background, a raised border, and a thickness of 10 pixels, and setting its text, font, and foreground color to "Smart College!!"
+titl.pack(fill=X) #Packing the label to fill the X axis
+l1 = tk.Label(window, image=logo1, bg="black",) #Creating a label with the logo image and setting its background color to black
+l1.place(x=470, y=10) # Placing the logo image at the position (470, 10) in the window
 
-titl = tk.Label(
-    window, text="Smart College!!", bg="black", fg="green", font=("arial", 27),
-)
-titl.place(x=525, y=12)
+titl = tk.Label( #
+    window, text="Smart College!!", bg="black", fg="green", font=("arial", 27), 
+) #Creating a label with the text "Smart College!!" and setting its background color, foreground color, font, and position
+titl.place(x=525, y=12) #Placing the label at the position (525, 12) in the window
 
-a = tk.Label(
-    window,
-    text="Welcome to the Face Recognition Based\nAttendance Management System",
-    bg="black",
-    fg="yellow",
-    bd=10,
-    font=("arial", 35),
-)
-a.pack()
+a = tk.Label( 
+    window, 
+    text="Welcome to the Face Recognition Based\nAttendance Management System", #
+    bg="black", 
+    fg="yellow", 
+    bd=10, 
+    font=("arial", 35), 
+) #Creating a label with a black background and a raised border, and setting its text, font, foreground color, and position
+a.pack() #Packing the label to fill the window
 
-ri = Image.open("UI_Image/register.png")
-r = ImageTk.PhotoImage(ri)
-label1 = Label(window, image=r)
-label1.image = r
-label1.place(x=100, y=270)
+ri = Image.open("UI_Image/register.png") #Opening the register image
+r = ImageTk.PhotoImage(ri) #Converting the register image to a PhotoImage object
+label1 = Label(window, image=r) # Creating a label with the register image
+label1.image = r #Saving the register image in the label1 variable
+label1.place(x=100, y=270) #Placing the register image at the position (100, 270) in the window
 
-ai = Image.open("UI_Image/attendance.png")
-a = ImageTk.PhotoImage(ai)
-label2 = Label(window, image=a)
-label2.image = a
-label2.place(x=980, y=270)
+ai = Image.open("UI_Image/attendance.png") #Opening the attendance image
+a = ImageTk.PhotoImage(ai) # Converting the attendance image to a PhotoImage object
+label2 = Label(window, image=a) #Creating a label with the attendance image
+label2.image = a #Saving the attendance image in the label2 variable
+label2.place(x=980, y=270) #Placing the attendance image at the position (980, 270) in the window
 
-vi = Image.open("UI_Image/verifyy.png")
-v = ImageTk.PhotoImage(vi)
-label3 = Label(window, image=v)
-label3.image = v
-label3.place(x=600, y=270)
+vi = Image.open("UI_Image/verifyy.png") #Opening the verify image
+v = ImageTk.PhotoImage(vi) #Converting the verify image to a PhotoImage object
+label3 = Label(window, image=v) # Creating a label with the verify image
+label3.image = v # Saving the verify image in the label3 variable
+label3.place(x=600, y=270) #Placing the verify image at the position (600, 270) in the window
 
 
 def TakeImageUI():
-    ImageUI = Tk()
-    ImageUI.title("Take Student Image..")
-    ImageUI.geometry("780x480")
-    ImageUI.configure(background="black")
-    ImageUI.resizable(0, 0)
-    titl = tk.Label(ImageUI, bg="black", relief=RIDGE, bd=10, font=("arial", 35))
+    ImageUI = Tk() # Create a new Tkinter window
+    ImageUI.title("Take Student Image..") # Create a new Tkinter window
+    ImageUI.geometry("780x480") # Set the window geometry
+    ImageUI.configure(background="black") # Configure the background color
+    ImageUI.resizable(0, 0) # Prevent the window from being resized
+    titl = tk.Label(ImageUI, bg="black", relief=RIDGE, bd=10, font=("arial", 35)) # Create a title label
     titl.pack(fill=X)
     # image and title
     titl = tk.Label(
         ImageUI, text="Register Your Face", bg="black", fg="green", font=("arial", 30),
-    )
+    )  # Add an image and title to the window
     titl.place(x=270, y=12)
-
-    # heading
+ # Create a heading label
     a = tk.Label(
         ImageUI,
         text="Enter the details",
@@ -156,7 +157,7 @@ def TakeImageUI():
     )
     a.place(x=280, y=75)
 
-    # ER no
+     # Create a label and entry for the Enrollment Number
     lbl1 = tk.Label(
         ImageUI,
         text="Enrollment No",
@@ -182,7 +183,7 @@ def TakeImageUI():
     txt1.place(x=250, y=130)
     txt1["validatecommand"] = (txt1.register(testVal), "%P", "%d")
 
-    # name
+     # Create a label and entry for the Name
     lbl2 = tk.Label(
         ImageUI,
         text="Name",
@@ -205,7 +206,7 @@ def TakeImageUI():
         font=("times", 25, "bold"),
     )
     txt2.place(x=250, y=200)
-
+    # Create a label for the notification
     lbl3 = tk.Label(
         ImageUI,
         text="Notification",
@@ -218,7 +219,7 @@ def TakeImageUI():
         font=("times new roman", 12),
     )
     lbl3.place(x=120, y=270)
-
+    # Create a notification message
     message = tk.Label(
         ImageUI,
         text="",
